b45469dab6c152e4c1b07f029bb47b62b6d12cf6
vipyne <vipyne@gmail.com>
2018-10-22 18:06:37 -0400

    initial commit

---
+++ iterator.kt
@@ -0,0 +1,11 @@
+package flat_tree
+
+class Iterator {
+
+  data class Iterator(val index: ULong, val offset: ULong, val factor: ULong)
+
+  fun index() {
+    return 7
+  }
+
+}
---
+++ test/iterator.kt
@@ -0,0 +1,8 @@
+import flat_tree.*
+import ktape.*
+
+val t = ktape
+
+val iterator = Iterator(0, 0, 0)
+
+t.ok(iterator.index())
